a3f74b5b98d5c770154202a57315a0f75c55d6e9
Matthias Patscheider <weisl12@hotmail.de>
2021-01-31 00:46:31 +0200

    added auto updater

--- preferences.py
+++ preferences.py
@@ -1,6 +1,7 @@
 import bpy
 import rna_keymap_ui
 
+from . import addon_updater_ops
 from .keymap import get_hotkey_entry_item
 
 
@@ -11,9 +12,63 @@ class VIEW3D_OT_renaming_preferences(bpy.types.AddonPreferences):
     # when defining this in a submodule of a python package.
     bl_idname = __package__  ### __package__ works on multifile and __name__ not
 
+    # addon updater preferences
+
+    auto_check_update = bpy.props.BoolProperty(
+        name="Auto-check for Update",
+        description="If enabled, auto-check for updates using an interval",
+        default=False,
+    )
+    updater_intrval_months = bpy.props.IntProperty(
+        name='Months',
+        description="Number of months between checking for updates",
+        default=0,
+        min=0
+    )
+    updater_intrval_days = bpy.props.IntProperty(
+        name='Days',
+        description="Number of days between checking for updates",
+        default=7,
+        min=0,
+        max=31
+    )
+    updater_intrval_hours = bpy.props.IntProperty(
+        name='Hours',
+        description="Number of hours between checking for updates",
+        default=0,
+        min=0,
+        max=23
+    )
+    updater_intrval_minutes = bpy.props.IntProperty(
+        name='Minutes',
+        description="Number of minutes between checking for updates",
+        default=0,
+        min=0,
+        max=59
+    )
+
     def draw(self, context):
         ''' simple preference UI to define custom inputs and user preferences'''
         layout = self.layout
+
+        # works best if a column, or even just self.layout
+        mainrow = layout.row()
+        col = mainrow.column()
+
+        # updater draw function
+        # could also pass in col as third arg
+        addon_updater_ops.update_settings_ui(self, context)
+
+        # Alternate draw function, which is more condensed and can be
+        # placed within an existing draw function. Only contains:
+        #   1) check for update/update now buttons
+        #   2) toggle for auto-check (interval will be equal to what is set above)
+        # addon_updater_ops.update_settings_ui_condensed(self, context, col)
+
+        # Adding another column to help show the above condensed ui as one column
+        # col = mainrow.column()
+        # col.scale_y = 2
+        # col.operator("wm.url_open","Open webpage ").url=addon_updater_ops.updater.website
 
         box = layout.box()
         split = box.split()
@@ -28,7 +83,6 @@ class VIEW3D_OT_renaming_preferences(bpy.types.AddonPreferences):
         kmis.append(get_hotkey_entry_item(km, 'utilities.cycle_cameras_backward'))
         kmis.append(get_hotkey_entry_item(km, 'VIEW3D_PT_tools_type_suffix'))
 
-
         for kmi in kmis:
             if kmi:
                 col.context_pointer_set("keymap", km)
@@ -37,9 +91,11 @@ class VIEW3D_OT_renaming_preferences(bpy.types.AddonPreferences):
                 col.label(text="No hotkey entry found")
                 col.operator("utilities.add_hotkey", text="Add hotkey entry", icon='ADD')
 
+
 classes = (
     VIEW3D_OT_renaming_preferences,
 )
+
 
 def register():
     from bpy.utils import register_class
