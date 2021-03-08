10b6a5f1c03fc76988e34ce90808a6a764ba5774
Matthias Patscheider <weisl12@hotmail.de>
2021-03-09 00:06:37 +0200

    Update Auto updater

--- addon_updater_ops.py
+++ addon_updater_ops.py
@@ -1340,12 +1340,12 @@ def register(bl_info):
 
 	# choose your own repository, must match git name for GitHUb and Bitbucket,
 	# for GitLab use project ID (numbers only)
-	updater.repo = "CameraManager"
+	updater.repo = "Cam-Manager"
 
 	#updater.addon = # define at top of module, MUST be done first
 
 	# Website for manual addon download, optional but recommended to set
-	updater.website = "https://github.com/Weisl/CameraManager"
+	updater.website = "https://github.com/Weisl/Cam-Manager"
 
 	# Addon subfolder path
 	# "sample/path/to/addon"
