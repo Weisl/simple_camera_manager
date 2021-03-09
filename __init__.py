33425e3681596e3201f16a74d7994bb1ea3c4c16
Matthias Patscheider <weisl12@hotmail.de>
2021-03-10 00:00:42 +0200

    update version number

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Cam-Manager",
     "author": "Matthias Patscheider",
-    "version": (0, 4),
+    "version": (1, 0),
     "blender": (2, 92, 0),
     "location": "Shift + C > (Cam Overview Panel), Alt + C > (Cam Adjustment Panel), Properties Panel > Scene > Quick Overview ",
     "description": "Tools for managing multiple cameras",
